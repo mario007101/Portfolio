bdaed9e478a5175fb18c594f402cbad031567648
mario007101 <gtamarkovic@gmail.com>
2023-06-21 14:03:48 +0200

    justified text

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -13,21 +13,13 @@ struct ContentView: View {
             
             VStack{
                 ScrollView {
-                    
-                    Text("Mário Markovič")
-                        .bold()
-                        .transparentText(color: .gray, lineWidth: 3)
-                        .mask(Color.white.opacity(0.7))
-                        .font(.system(size: 35))
-                        .padding(.horizontal, -170)
-
-                    
-                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, nulla ut ultrices gravida, libero dui elementum risus, vitae efficitur sem nunc et purus. Sed interdum, nisi eget vulputate ullamcorper, orci leo interdum erat, nec faucibus augue eros non erat. Ut vestibulum bibendum ipsum sit amet luctus. Integer vitae efficitur leo, quis porttitor erat.")
-                        .frame(maxWidth: .minimum(350, 450))
-                        .font(.system(size: 15))
-                        .foregroundColor(.cyan)
-                        .padding(.top, -5)
-                        
+                        Text("Mário Markovič")
+                            .bold()
+                            .transparentText(color: .gray, lineWidth: 3)
+                            .mask(Color.white.opacity(0.7))
+                            .font(.system(size: 35))
+                            .padding(.horizontal, -170)
+                        InformationAbout() //Have to finish
                 }
             }
             .frame(maxHeight: .infinity, alignment: .center)
---
+++ Portfolio/InformationAbout.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct InformationAbout: View {
+    var body: some View {
+        
+        JustifiedText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, nulla ut ultrices gravida, libero dui elementum risus, vitae efficitur sem nunc et purus. Sed interdum, nisi eget vulputate ullamcorper, orci leo interdum erat, nec faucibus augue eros non erat. Ut vestibulum bibendum ipsum sit amet luctus. Integer vitae efficitur leo, quis porttitor erat.")
+    }
+}
+
+struct JustifiedText: UIViewRepresentable {
+  private let text: String
+  private let font: UIFont
+
+  init(_ text: String, font: UIFont = .systemFont(ofSize: 18)) {
+    self.text = text
+    self.font = font
+  }
+
+  func makeUIView(context: Context) -> UITextView {
+    let textView = UITextView()
+    textView.font = font
+    textView.textAlignment = .justified
+    return textView
+  }
+
+  func updateUIView(_ uiView: UITextView, context: Context) {
+    uiView.text = text
+  }
+}
+
+struct InformationAbout_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationAbout()
+    }
+}
