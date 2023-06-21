c2125703b9af72e1e24b8dc87624fcdd23f5f8fa
mario007101 <gtamarkovic@gmail.com>
2023-06-21 11:51:35 +0200

    changes

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -13,15 +13,25 @@ struct ContentView: View {
             
             VStack{
                 ScrollView {
+                    
                     Text("Mário Markovič")
                         .bold()
-                        .transparentText(color: .teal, lineWidth: 3)
+                        .transparentText(color: .gray, lineWidth: 3)
                         .mask(Color.white.opacity(0.7))
-                        .font(.system(size: 55))
+                        .font(.system(size: 35))
+                        .padding(.horizontal, -170)
+
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, nulla ut ultrices gravida, libero dui elementum risus, vitae efficitur sem nunc et purus. Sed interdum, nisi eget vulputate ullamcorper, orci leo interdum erat, nec faucibus augue eros non erat. Ut vestibulum bibendum ipsum sit amet luctus. Integer vitae efficitur leo, quis porttitor erat.")
+                        .frame(maxWidth: .minimum(350, 450))
+                        .font(.system(size: 15))
+                        .foregroundColor(.cyan)
+                        .padding(.top, -5)
+                        
                 }
             }
-            .frame(maxHeight: .infinity, alignment: .top)
-            .padding(.vertical)
+            .frame(maxHeight: .infinity, alignment: .center)
+            .border(.pink)
         }
     }
 }
