071eff04fe3e17f2204414be309b2f5c2b330dd7
mario007101 <gtamarkovic@gmail.com>
2023-06-22 19:28:11 +0200

    text in scrollview

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -2,26 +2,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         ZStack{
-            
             Image("space")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(minWidth: 0, maxWidth: .infinity)
                 .edgesIgnoringSafeArea(.all)
-            
-            VStack{
-                ScrollView {
+                VStack {
+                    ScrollView {
                         Text("Mário Markovič")
                             .bold()
                             .transparentText(color: .gray, lineWidth: 3)
                             .mask(Color.white.opacity(0.7))
                             .font(.system(size: 35))
                             .padding(.horizontal, -170)
-                        InformationAbout() //Have to finish
+                        InformationAbout()
+                            
+                    }
                 }
-            }
             .frame(maxHeight: .infinity, alignment: .center)
             .border(.pink)
         }
--- Portfolio/InformationAbout.swift
+++ Portfolio/InformationAbout.swift
@@ -2,8 +2,9 @@ import SwiftUI
 
 struct InformationAbout: View {
     var body: some View {
-        
-        JustifiedText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, nulla ut ultrices gravida, libero dui elementum risus, vitae efficitur sem nunc et purus. Sed interdum, nisi eget vulputate ullamcorper, orci leo interdum erat, nec faucibus augue eros non erat. Ut vestibulum bibendum ipsum sit amet luctus. Integer vitae efficitur leo, quis porttitor erat.")
+        NavigationView {
+                JustifiedText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, nulla ut ultrices gravida, libero dui elementum risus, vitae efficitur sem nunc et purus. Sed interdum, nisi eget vulputate ullamcorper, orci leo interdum erat, nec faucibus augue eros non erat. Ut vestibulum bibendum ipsum sit amet luctus. Integer vitae efficitur leo, quis porttitor erat.")
+        }
     }
 }
 
@@ -27,6 +28,7 @@ struct JustifiedText: UIViewRepresentable {
     uiView.text = text
   }
 }
+
 
 struct InformationAbout_Previews: PreviewProvider {
     static var previews: some View {
