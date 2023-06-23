be3671ba6030617962780dde4edf4dacd0b8678d
mario007101 <gtamarkovic@gmail.com>
2023-06-23 17:57:41 +0200

    added circles

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -8,18 +8,38 @@ struct ContentView: View {
                     .resizable()
                     .frame(minWidth: 0, maxWidth: .infinity)
                     .edgesIgnoringSafeArea(.all)
+                
+                
+                Circle()
+                    .foregroundColor(.gray)
+                    .position(x: 180, y: -320)
+                    .frame(width: 60, height: 60)
                     VStack {
                         ScrollView {
+                            
+                            Circle()
+                                .foregroundColor(.gray)
+                                .frame(width: 50, height: 50)
+                                .position(x: 40, y: 20)
+                            Circle()
+                                .foregroundColor(.gray)
+                                .frame(width: 50, height: 50)
+                                .position(x: 60, y: -10)
+                                .opacity(0.7)
+                            
                             Text("Mário Markovič")
                                 .bold()
                                 .transparentText(color: .gray, lineWidth: 4)
                                 .mask(Color.white.opacity(0.7))
                                 .font(.system(size: 35))
                                 .padding(.horizontal, -170)
+                                .padding(.vertical, -110)
                             
+    
                             InformationAbout()
-                                .padding(.top, 2)
+                                .padding(.top, -50)
                                 
+                    
                         }
                     }
                 .frame(maxHeight: .infinity, alignment: .center)
